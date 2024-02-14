5201bf5e63a120892078af1dee38d2072b1f373a
MauryaAayush <maayush235@gmail.com>
2024-02-15 00:37:39 +0530

    Radhey Radhey

--- lib/view/homescreen.dart
+++ lib/view/homescreen.dart
@@ -62,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ],
                       ),
 
-                      SizedBox(width: 70,),
+                      const SizedBox(width: 70,),
 
                       Icon(
                         Icons.notifications_none_outlined,
