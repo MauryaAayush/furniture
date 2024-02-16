bf2229491d5295ad3036fd52bbdcda97eed6b0cd
MauryaAayush <maayush235@gmail.com>
2024-02-16 09:00:10 +0530

    Radhey Radhey

--- lib/view/productScreen.dart
+++ lib/view/productScreen.dart
@@ -231,7 +231,7 @@ class _ProductScreenState extends State<ProductScreen> {
                         ),)
                       ],
                     ),
-                    SizedBox.square(dimension: 15),
+                    const SizedBox.square(dimension: 15),
 
                     Divider(thickness: 1),
 
